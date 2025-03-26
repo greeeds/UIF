ee76deb0e65dae0e40b767c1931e8faea17dc56c
greed <greedisgoodlsh@gmail.com>
2025-03-26 11:01:38 +0800

    auto update 1

--- uifd/uif/http_get.go
+++ uifd/uif/http_get.go
@@ -31,7 +31,6 @@ func getEncoding(charset string) (encoding.Encoding, error) {
 }
 
 func GetProxyHTTPUA() string {
-
 	return "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0"
 }
 
